94abf14e834f334515a24ea8886778f9dd8b3f05
Samantha Brennan <samanthabrennan@usf.edu>
2025-10-19 13:40:48 -0400

    Initialize Repository

---
+++ cp_1.js
@@ -0,0 +1,4 @@
+//Step 2: Create form Layout in HTML
+//Step 3: Use DOM event listeners
+//Step 4: Use evetn bubbling & delegation to manage events of all input fields
+//Step 5: Prevent background clicks
