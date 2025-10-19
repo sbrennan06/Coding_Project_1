ea5a9d934012b0be3136a83620de11b93b5d2a37
Samantha Brennan <samanthabrennan@usf.edu>
2025-10-19 15:06:47 -0400

    completed character count function and style

--- cp_1.js
+++ cp_1.js
@@ -5,7 +5,14 @@ const counter = document.createElement(`span`);
 counter.id = `comments-count`;
 counter.textContent = `Characters: 0`;
 comments.insertAdjacentElement(`afterend`, counter);
+counter.insertAdjacentElement(`afterend`, document.createElement(`br`));
 
+function updateCount() {
+    counter.textContent = `Characters: ${comments.value.length}`;
+}
+
+comments.addEventListener(`input`, updateCount);
+updateCount();
 
 
 //Step 4: Use evetn bubbling & delegation to manage events of all input fields
