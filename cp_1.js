22aed22555242c2f9781b016edf8d60fadd5702c
Samantha Brennan <samanthabrennan@usf.edu>
2025-10-19 14:45:12 -0400

    add character counter in JS

--- cp_1.js
+++ cp_1.js
@@ -1,4 +1,12 @@
-//Step 2: Create form Layout in HTML
 //Step 3: Use DOM event listeners
+//step 3.1: count characters as users type
+const comments = document.getElementById(`comments`);
+const counter = document.createElement(`span`);
+counter.id = `comments-count`;
+counter.textContent = `Characters: 0`;
+comments.insertAdjacentElement(`afterend`, counter);
+
+
+
 //Step 4: Use evetn bubbling & delegation to manage events of all input fields
 //Step 5: Prevent background clicks
